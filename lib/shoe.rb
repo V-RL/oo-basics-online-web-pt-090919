e5b1d798be81023932b3e1e093f41a29d953b1e3
Brandon Furr <brandon.furr123@gmail.com>
2019-10-02 00:20:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble(fixed)
+    if Shoe.condition == old 
+      Shoe.condition = new
+    puts "#{fixed}"
+    
+  end
+end
+end
